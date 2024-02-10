6eeaf9e0c69d47159a9bf6fb7df58dce4e4c62eb
Vladislav Pavlov <vpavlov.dev@gmail.com>
2024-02-10 14:59:56 +0300

    0931. Minimum Falling Path Sum

---
+++ Medium/0931. Minimum Falling Path Sum.swift
@@ -0,0 +1,42 @@
+// Solution by Vladislav Pavlov
+// Difficulty: Medium
+// Topics: Array, Dynamic Programming, Matrix
+//
+//Given an n x n array of integers matrix, return the minimum sum of any falling path through matrix.
+//
+//A falling path starts at any element in the first row and chooses the element in the next row that is either directly below or diagonally left/right. Specifically, the next element from position (row, col) will be (row + 1, col - 1), (row + 1, col), or (row + 1, col + 1).
+//
+// 
+//
+//Example 1:
+//
+//
+//Input: matrix = [[2,1,3],[6,5,4],[7,8,9]]
+//Output: 13
+//Explanation: There are two falling paths with a minimum sum as shown.
+//Example 2:
+//
+//
+//Input: matrix = [[-19,57],[-40,-5]]
+//Output: -59
+//Explanation: The falling path with a minimum sum is shown.
+
+class Solution {
+    func minFallingPathSum(_ matrix: [[Int]]) -> Int {
+        let matrixSize = matrix.count
+        var resultMatrix = matrix[matrix.count-1]
+        
+        for row in 1..<matrixSize {
+            var temp = resultMatrix
+            for col in 0..<matrixSize{
+                let left = col == 0 ? Int.max : resultMatrix[col - 1]
+                let mid = resultMatrix[col]
+                let right = col == (matrixSize - 1) ? Int.max : resultMatrix[col + 1]
+                temp[col] = min(left, mid, right) + matrix[matrix.count-1-row][col]
+            }
+            resultMatrix = temp
+        }
+        
+        return resultMatrix.min()!
+    }
+}
