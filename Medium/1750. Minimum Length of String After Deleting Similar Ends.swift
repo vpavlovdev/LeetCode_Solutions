7bb4a482794e045f8799dd157ba4a2c639e42d47
Vladislav Pavlov <vpavlov.dev@gmail.com>
2024-03-05 14:59:10 +0300

    1750. Minimum Length of String After Deleting Similar Ends

---
+++ Medium/1750. Minimum Length of String After Deleting Similar Ends.swift
@@ -0,0 +1,63 @@
+// Solution by Vladislav Pavlov
+// Difficulty: Medium
+// Topics: Two Pointers, String
+//
+//Given a string s consisting only of characters 'a', 'b', and 'c'. You are asked to apply the following algorithm on the string any number of times:
+//
+//Pick a non-empty prefix from the string s where all the characters in the prefix are equal.
+//Pick a non-empty suffix from the string s where all the characters in this suffix are equal.
+//The prefix and the suffix should not intersect at any index.
+//The characters from the prefix and suffix must be the same.
+//Delete both the prefix and the suffix.
+//Return the minimum length of s after performing the above operation any number of times (possibly zero times).
+//
+// 
+//
+//Example 1:
+//
+//Input: s = "ca"
+//Output: 2
+//Explanation: You can't remove any characters, so the string stays as is.
+//Example 2:
+//
+//Input: s = "cabaabac"
+//Output: 0
+//Explanation: An optimal sequence of operations is:
+//- Take prefix = "c" and suffix = "c" and remove them, s = "abaaba".
+//- Take prefix = "a" and suffix = "a" and remove them, s = "baab".
+//- Take prefix = "b" and suffix = "b" and remove them, s = "aa".
+//- Take prefix = "a" and suffix = "a" and remove them, s = "".
+//Example 3:
+//
+//Input: s = "aabccabba"
+//Output: 3
+//Explanation: An optimal sequence of operations is:
+//- Take prefix = "aa" and suffix = "a" and remove them, s = "bccabb".
+//- Take prefix = "b" and suffix = "bb" and remove them, s = "cca".
+
+import Collections
+
+class Solution {
+    @_optimize(speed)
+    func minimumLength(_ s: String) -> Int {
+        guard s.count > 1 else { return s.count }
+        
+        var result = Deque(s)
+        
+        while result.count > 1 {
+            guard result.first == result.last else { break }
+            
+            let letter = result.first
+            
+            while result.first == letter {
+                result.removeFirst()
+            }
+            
+            while result.last == letter {
+                result.removeLast()
+            }
+        }
+        
+        return result.count
+    }
+}
