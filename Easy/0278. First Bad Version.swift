49d587166c88e5ac6b8a6efbaafd8d70b5d64f88
Vladislav Pavlov <vpavlov.dev@gmail.com>
2024-03-29 19:13:56 +0300

    0278. First Bad Version

---
+++ Easy/0278. First Bad Version.swift
@@ -0,0 +1,45 @@
+// Solution by Vladislav Pavlov
+// Difficulty: Easy
+// Topics: Binary Search, Interactive
+//
+//You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
+//
+//Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
+//
+//You are given an API bool isBadVersion(version) which returns whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.
+//
+//Example 1:
+//Input: n = 5, bad = 4
+//Output: 4
+//Explanation:
+//call isBadVersion(3) -> false
+//call isBadVersion(5) -> true
+//call isBadVersion(4) -> true
+//Then 4 is the first bad version.
+//
+//Example 2:
+//Input: n = 1, bad = 1
+//Output: 1
+
+/**
+ * The knows API is defined in the parent class VersionControl.
+ *     func isBadVersion(_ version: Int) -> Bool{}
+ */
+
+class Solution : VersionControl {
+    func firstBadVersion(_ n: Int) -> Int {
+
+        func firstBadVersion(from: Int, to: Int) -> Int {
+            let mid = (from + to) / 2
+            
+            if isBadVersion(mid) {
+                if !isBadVersion(mid - 1) { return mid }
+                return firstBadVersion(from: from, to: mid - 1)
+            } else {
+                return firstBadVersion(from: mid + 1, to: to)
+            }
+        }
+        
+        return firstBadVersion(from: 1, to: n)
+    }
+}
